6003579242d8899387f66d2cbc0dd2c868a670a7
caojianbo <caojianbo@taptap.com>
2020-12-21 10:40:18 +0800

    添加登录功能

--- TdsDemo/ViewController.m
+++ TdsDemo/ViewController.m
@@ -6,17 +6,63 @@
 //
 
 #import "ViewController.h"
+#import <TapSDK/TapSDK.h>
+#import <TapSDK/TapLoginHelper.h>
 
-@interface ViewController ()
+@interface ViewController ()<TapLoginResultDelegate>
+- (void) initTapSDK;
 
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UIButton *loginButton = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 50)];
+    [loginButton setTitle:@"Login" forState:UIControlStateNormal];
+    [loginButton setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
+    [loginButton addTarget:self action:@selector(taptapLogin:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:loginButton];
+    
+    [self initTapSDK];
 }
+
+- (void) initTapSDK {
+    
+    TDSConfig *config = TDSConfig.new;
+    config.clientId = @"FwFdCIr6u71WQDQwQN";
+    
+    [TDSInitializer initWithConfig:config];
+    
+    // 开启 TapDB
+    [TDSInitializer enableTapDBWithChannel:@"default" gameVersion:@"1.0.0"];
+        
+    // 开启 动态
+    [TDSInitializer enableMoment];
+    
+}
+
+- (void) taptapLogin:(UIButton *) button {
+    [TapLoginHelper registerLoginResultDelegate:self];
+    [TapLoginHelper startTapLogin:@[@"public_profile"]]
+}
+
+
+- (void)onLoginSuccess:(TTSDKAccessToken *)token {
+    
+}
+
+- (void)onLoginCancel {
+    
+}
+
+- (void)onLoginError:(AccountGlobalError *)error{
+    
+}
+
+
 
 
 @end
